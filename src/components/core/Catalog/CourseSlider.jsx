3e09df48f1881034da834145f5d68658153473b2
mohd-momin <mohammadmomin_se21a14_53@dtu.ac.in>
2024-07-10 22:39:32 +0530

    commiting files

---
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, FreeMode, Navigation, Pagination } from 'swiper';
+
+import Course_Card from './Course_Card';
+
+const CourseSlider = ({ Courses }) => {
+  return (
+    <>
+      {Courses?.length ? (
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={25}
+          centeredSlides={false}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          // navigation={true}
+          modules={[Autoplay, FreeMode, Pagination, Navigation]}
+          breakpoints={{
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          className="max-h-[30rem]"
+        >
+          {Courses?.map((course, i) => (
+            <SwiperSlide key={i}>
+              <Course_Card course={course} Height={"h-[250px]"} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p className="text-xl text-richblack-5">No Course Found</p>
+      )}
+    </>
+  );
+};
+
+export default CourseSlider;
