7525942af345f7eef07449c06166e0cab87b5ed4
mohd-momin <mohammadmomin_se21a14_53@dtu.ac.in>
2024-07-11 11:04:46 +0530

    commit

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -238,24 +238,26 @@ function CourseDetails() {
             </div>
 
             {/* Author Details */}
-            <div className="mb-12 py-4">
-              <p className="text-[28px] font-semibold">Author</p>
-              <div className="flex items-center gap-4 py-4">
-                <img
-                  src={
-                    instructor.image
-                      ? instructor.image
-                      : `https://api.dicebear.com/5.x/initials/svg?seed=${instructor.firstName} ${instructor.lastName}`
-                  }
-                  alt="Author"
-                  className="h-14 w-14 rounded-full object-cover"
-                />
-                <p className="text-lg">{`${instructor.firstName} ${instructor.lastName}`}</p>
+            {instructor && (
+              <div className="mb-12 py-4">
+                <p className="text-[28px] font-semibold">Author</p>
+                <div className="flex items-center gap-4 py-4">                
+                  <img
+                    src={
+                      instructor.image
+                        ? instructor.image
+                        : `https://api.dicebear.com/5.x/initials/svg?seed=${encodeURIComponent(instructor.firstName + ' ' + instructor.lastName)}`
+                    }
+                    alt="Author"
+                    className="h-14 w-14 rounded-full object-cover"
+                  />
+                  <p className="text-lg">{`${instructor.firstName} ${instructor.lastName}`}</p>
+                </div>
+                <p className="text-richblack-50">
+                  {instructor.additionalDetails?.about || ''}
+                </p>
               </div>
-              <p className="text-richblack-50">
-                {instructor?.additionalDetails?.about}
-              </p>
-            </div>
+            )}
           </div>
         </div>
       </div>
